1d23076534caba8cc60507f6c2100dfb72fd681f
ian <ianfmc@gmail.com>
2014-10-26 23:43:54 +0900

    Minor cleanup and complete data set. Final Version

--- assignment2.R
+++ assignment2.R
@@ -22,8 +22,7 @@ assignment2 <- function() {
   return(x)
 }
 
-data.processing.do <- function(location) {
-  
+data.processing.do <- function(location) {  
   data <- data.processing.read(location)
   data$BGN_DATE <- data.processing.convert.dates(data$BGN_DATE)
   data <- transform(data, YEAR = data.processing.get.year(data$BGN_DATE))
@@ -32,13 +31,12 @@ data.processing.do <- function(location) {
 }
 
 data.processing.read <- function(location) {
-  
   zipped.file.name <- "repdata-data-StormData.csv.bz2"  
   if (!file.exists(zipped.file.name)) {    
     download(location, zipped.file.name, quiet=FALSE)          
   }   
   bz.con <- bzfile(zipped.file.name)
-  data <- read.csv(bz.con, nrows=400000) ## be sure to change this before final
+  data <- read.csv(bz.con) ## be sure to change this before final
   
   return(data)
 }
@@ -52,32 +50,28 @@ data.processing.get.year <- function(date) {
 }
 
 analysis.do <- function(wx.df) {
-  
   fatalities <- ddply(wx.df, .(EVTYPE), summarise, total=sum(FATALITIES, na.rm=TRUE))
   damage <- ddply(wx.df, .(EVTYPE), summarise, total=sum(PROPDMG, na.rm=TRUE))
 
   impact <- cbind(fatalities, damage)
-  impact[, 3] <- NULL
-  
+  impact[, 3] <- NULL  
   colnames(impact) <- c("Event", "Total.Fatalities", "Total.Damage")
+
   return(impact)
 }
 
 analysis.cross.tabulate <- function(wx.summ) {
-
   sort.by.fatalities <- wx.summ[ order(wx.summ$Total.Fatalities, decreasing=TRUE), ]
   sort.by.fatalities <- sort.by.fatalities[1:10, ]
   
   sort.by.damage <- wx.summ[ order(wx.summ$Total.Damage, decreasing=TRUE), ]
-  sort.by.damage <- sort.by.damage[1:10, ]
-  
+  sort.by.damage <- sort.by.damage[1:10, ]  
   cross.tab <- merge(sort.by.damage, sort.by.fatalities)
+
   return(cross.tab)
 }
 
 analysis.plot <- function(data, t="Analysis") {
-  
-  ## png("analysis.png", width=800, height=600)
   p <- ggplot(data, aes(x=Total.Fatalities, y=(Total.Damage/1000), color=Event))
   p <- p + geom_point(size=6)
   p <- p + xlab("Total Fatalities")
@@ -85,6 +79,5 @@ analysis.plot <- function(data, t="Analysis") {
   p <- p + ggtitle(t)
   
   print(p)
-  ## dev.off()
 }
 
